a46a7b9f353ff8e4e1fe22df5c1119815c355864
陈碧滔 <bitao.chen@envisioncn.com>
2017-01-17 16:18:46 +0800

    提交代码，增加忽略列表

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
 
+//定义一个testLess任务（自定义任务名称）
+gulp.task('dist', function () {
+    gulp.src('src/angular-echarts.js') //该任务针对的文件
+        .pipe(gulp.dest('dist/')); //将会在src/css下生成index.css
+});
+ 
+gulp.task('default',['dist']);
+/*
 gulp.task('default', function() {
 	// 将你的默认的任务代码放在这
 });
+*/
