00e65e260caa4cba88f381f8594fbdb5ba1cfc0b
陈碧滔 <bitao.chen@envisioncn.com>
2017-01-18 11:43:44 +0800

    可以展示图表了

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	del = require('del');
 
-//定义一个testLess任务（自定义任务名称）
-gulp.task('dist', function () {
-    gulp.src('src/angular-echarts.js') //该任务针对的文件
-        .pipe(gulp.dest('dist/')); //将会在src/css下生成index.css
+
+gulp.task('clean', function(cb) {
+	del(['dist/*'], cb)
 });
- 
-gulp.task('default',['dist']);
+
+gulp.task('minifyjs', function() {
+	return gulp.src('src/*.js')
+		.pipe(concat('angular-echarts.js'))    //合并所有js
+		.pipe(gulp.dest('dist/'))    //输出main.js到文件夹
+		.pipe(rename({suffix: '.min'}))   //rename压缩后的文件名
+		.pipe(uglify())    //压缩
+		.pipe(gulp.dest('dist/'));  //输出
+});
+
+gulp.task('default', ['clean', 'minifyjs']);
 /*
 gulp.task('default', function() {
-	// 将你的默认的任务代码放在这
+	gulp.start('clean', 'minifyjs');
 });
 */
